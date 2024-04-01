94a467212bf7fbe76ddf3ea24a2ded3aac4f3999
user <danyakovin123@mail.ru>
2024-04-01 10:06:31 +0300

    1 Задача

--- Kovinskiy_0.1/Program.cs
+++ Kovinskiy_0.1/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Kovinskiy_0._1
+﻿using System;
+
+namespace Kovinskiy_0._1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+            Console.WriteLine("Введите кол-во у вас золото: ");
+            int UserValue = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Вы желаете купить 30 кристалов");
+            int CrystalPrice = 30;
+            
+            int remainder = UserValue - CrystalPrice;
+            
+            bool canBuy = UserValue >= CrystalPrice;
+
+            Console.WriteLine(canBuy ? $"Сделка завершена\nВаш остаток: {remainder}" : "У вас меньше, чем нужно");
         }
     }
 }
